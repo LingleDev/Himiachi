7bf8d3497e3fedfc0e5d7e4791fdb59347725072
FHGDev <gideon.lingle14@gmail.com>
2018-04-20 15:37:29 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,9 +2,11 @@ module.exports.run = (bot, message, args, discord) => {
   let embed = new discord.RichEmbed()
   .setTitle("Himiachi Ping")
   .setDescription(`Pong! My ping is ${bot.ping}ms.`)
+  .setThumbnail(bot.user.avatarURL)
   .setTimestamp()
+  .setFooter(message.author.avatarURL, message.author.username)
   .setColor("BLUE")
-  console.log(`${message.author.username} used the ping command.`)
+  message.channel.send({embed: embed })
 }
 
 module.exports.help = {
