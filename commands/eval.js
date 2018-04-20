ef3afd8a5c286f90543244d355e0c90348c62be3
FHGDev <gideon.lingle14@gmail.com>
2018-04-20 17:41:21 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
+const ownerids = require('../config.json').ownerids
 
 module.exports.run = (bot, message, args) => {
+    if (ownerids.includes(message.author.id)) {
     const content = message.content.split(' ').slice(1).join(' ');
   const result = new Promise((resolve, reject) => resolve(eval(content)));
 
@@ -15,7 +17,10 @@ module.exports.run = (bot, message, args) => {
     if (err.includes(bot.token)) err = err.replace(bot.token, 'Not for your eyes');
 
     return message.channel.send(err, { code: 'js' })
-  });  
+  }); 
+    } else {
+        message.channel.send("Owner only, you cannot use this command.")
+    }
   }
 
 module.exports.help = {
