75daa11ffe437a33f621071e0cb3e5c2e8f22b4a
FHGDev <gideon.lingle14@gmail.com>
2018-04-20 15:00:00 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,3 +3,14 @@ bot = new discord.Client(),
 config = require('./config.json'),
 prefix = process.env.prefix;
 bot.login(process.env.token);
+
+bot.on('ready', () => {
+  bot.user.setActivity(`for .help | ${bot.guilds.size} servers`, {type: "WATCHING"})
+  console.log("Himiachi ready!")
+})
+
+bot.on('message', message => {
+  if (message.content == prefix+"help") {
+    message.channel.send("I'm a fairly new bot, do you really think I have any commands yet?").then(m => m.delete(10000))
+  }
+})
