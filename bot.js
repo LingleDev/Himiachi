30b9f73369d21ffcf3d6357dc9a9f0050269498e
FHGDev <gideon.lingle14@gmail.com>
2018-04-20 15:27:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,30 @@ config = require('./config.json'),
 prefix = process.env.prefix;
 bot.login(process.env.token);
 
+// ===Loading commands===
+
+console.log("Loading commands...")
+bot.commands = new discord.Collection()
+
+require('fs').readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+// ===Done Loading commands===
+
 bot.on('ready', () => {
   bot.user.setActivity(`for .help | ${bot.guilds.size} servers`, {type: "WATCHING"})
   console.log("Himiachi ready!")
 })
 
 bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = message.content.slice(1)
+  
   if (message.content == prefix+"help") {
     message.channel.send("I'm a fairly new bot, do you really think I have any commands yet?").then(m => m.delete(10000))
   }
