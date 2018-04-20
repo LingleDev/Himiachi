bca087e0f0400d4dd3d41563598b3b90acfc4a39
FHGDev <gideon.lingle14@gmail.com>
2018-04-20 16:29:21 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,6 @@
 module.exports.run = (bot, message, args, discord) => {        
     let member = message.mentions.members.first();
-    let reason = args[1]
+    let reason = args.join(' ');
     if (!args) {
         member.ban().then(message.channel.send(`Okay, just banned ${member.displayName}.`))
     } else {
