cc5866e4731eb833a7d8fbeeaf1b9de7ac84dac2
FHGDev <gideon.lingle14@gmail.com>
2018-04-20 17:26:35 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const discord = require('discord.js'),
 bot = new discord.Client(),
 config = require('./config.json'),
-prefix = process.env.prefix;
+prefix = process.env.prefix,
+{baselogger} = require('./logger.js')
 bot.login(process.env.token);
 
 // ===Loading commands===
