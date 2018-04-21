67d3c4f643e10311e4b8f25ad8c6727653cce21f
FHGDev <gideon.lingle14@gmail.com>
2018-04-21 17:22:42 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,4 +1,4 @@
-module.exports.run = (bot, message, args, discord) => 
+module.exports.run = (bot, message, args, discord) => {
  let em = new discord.RichEmbed()
   .setTitle("Himiachi Invite")
   .setDescription(`https://discordapp.com/api/oauth2/authorize?client_id=410101842329075712&permissions=8&scope=bot`)
@@ -6,6 +6,8 @@ module.exports.run = (bot, message, args, discord) =>
   .setThumbnail(bot.user.avatarURL)
   .setFooter(message.author.username)
   .setTimestamp()
+
+   message.channel.send({embed: em})
   }
 
 module.exports.help = {
