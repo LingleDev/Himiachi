c429a5d27bc3ca1143d4a26776f38f094a7ef41a
FHGDev <gideon.lingle14@gmail.com>
2018-04-21 11:07:56 -0500

    Create contact.js

---
+++ commands/contact.js
@@ -0,0 +1,20 @@
+module.exports.run = (bot, message, args, discord) => {
+    var mes = args.join(' ')
+    let em = new discord.RichEmbed()
+    .setTitle(`Contact System`)
+    .setDescription(mes)
+    .setColor(`BLUE`)
+    .setFooter(`This message was sent by ${message.author.tag}`)
+    .setTimestamp()
+    .setThumbnail(bot.user.avatarURL)
+    
+    let emb = new discord.RichEmbed()
+    .setTitle(`Contact System`)
+    .setDescription(`Now contacting Wolfy and FHGDev...`)
+    .setColor(`BLUE`)
+    .setFooter(`Contact System`)
+    .setTimestamp()
+    .setThumbnail(bot.user.avatarURL)
+    message.channel.send({embed: emb})
+    bot.users.get('276040277293268992').send({embed: em})
+}
