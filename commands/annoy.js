87922ff69b49b6a2047fa13cfa560352539ac6f9
FHGDev <gideon.lingle14@gmail.com>
2018-04-21 10:59:50 -0500

    Update annoy.js

--- commands/annoy.js
+++ commands/annoy.js
@@ -1,9 +1,17 @@
 module.exports.run = (bot, message, args, discord) => {
+ let member = message.mentions.members.first();
  let em = new discord.RichEmbed()
-  .setTitle("Comeing Soon!")
-  .setDescription("This command is comeing soon!")
+  .setTitle("Himiachi Annoy")
+  .setDescription(`Now annoying <@${member.id}>...`)
   .setColor("RED")
   .setThumbnail(bot.user.avatarURL)
   .setFooter(message.author.username)
   .setTimestamp()
+   bot.users.get(member.id).send(`<@${member.id}> LOL! ${message.author.username} sent me to annoy you! <@${member.id}> <@${member.id}>`)
    message.channel.send({embed: em})
+}
+
+module.exports.help = {
+ name: "annoy",
+ usage: ".annoy [@member]"
+}
