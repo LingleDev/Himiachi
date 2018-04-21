8d79309097b0aaf53c1feda8517f78f65a81dc99
XXWolfBane <33186976+XXWolfBane@users.noreply.github.com>
2018-04-20 23:25:47 -0400

    TO ANNOY JAMES

--- commands/annoy.js
+++ commands/annoy.js
@@ -1,1 +1,9 @@
-
+module.exports.run = (bot, message, args, discord) => {
+ let em = new discord.RichEmbed()
+  .setTitle("Comeing Soon!")
+  .setDescription("This command is comeing soon!")
+  .setColor("RED")
+  .setThumbnail(bot.user.avatarURL)
+  .setFooter(message.author.username)
+  .setTimestamp()
+   message.channel.send({embed: em})
