391607b30c65d5454899fc0e46a3eca9ad923469
FHGDev <gideon.lingle14@gmail.com>
2018-04-21 11:52:43 -0500

    Made the restart command.

---
+++ commands/restart.js
@@ -0,0 +1,20 @@
+module.exports.run = (bot, message, args, discord) => {
+let em = new discord.RichEmbed()
+.setTitle("Himiachi Restarting...")
+.setDescription(":wave: Restarting...")
+.setThumbnail(bot.user.avatarURL)
+.setTimestamp()
+.setColor("GOLD")
+
+  function restart(channel) {
+    channel.send({embed: em})
+    .then(() => bot.destroy())
+    .then(() => bot.login(process.env.token))
+  }
+  
+  restart(message.channel)
+}
+
+module.exports.help = {
+  name: "restart"
+}
