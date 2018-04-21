9d8feac54c74646ce4e422cc5eaa4f84e36e350e
FHGDev <gideon.lingle14@gmail.com>
2018-04-20 19:43:52 -0500

    Create uptime.js

---
+++ commands/uptime.js
@@ -0,0 +1,21 @@
+module.exports.run = (bot, message, args, discord) => {
+  var msgArray = [];
+  var hrs = Math.round(bot.uptime / (1000 * 60 * 60)) + " hour(s),"
+  var mins = " " + Math.round(bot.uptime / (1000 * 60)) % 60 + " minute(s), "
+  var sec = Math.round(bot.uptime / 1000) % 60 + " second(s)"
+  if (hrs == "0 hour(s),") hrs = ""
+  if (mins == " 0 minute(s), ") mins = ""
+  msgArray.push(hrs + mins + sec)
+  let embed = new discord.RichEmbed()
+  .setTitle(`Himiachi Uptime`)
+  .setDescription(`Serving ${bot.guilds.size} guilds, ${bot.channels.size} channels, and ${bot.users.size} users for ${msgArray}.`)
+  .setTimestamp()
+  .setFooter(`Himiachi Uptime`)
+  message.channel.send({embed: em})
+}
+
+module.exports.help = {
+  name: "uptime",
+  usage: ``,
+  information: ""
+}
