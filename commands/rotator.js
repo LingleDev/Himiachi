a521257f729bcce925ddcad876544ffbfd36263e
FHGDev <gideon.lingle14@gmail.com>
2018-04-22 10:22:46 -0500

    Create rotator.js

---
+++ commands/rotator.js
@@ -0,0 +1,27 @@
+let config = require('../config.json')
+let offon = "on"
+
+module.exports.run = (bot, message, args, discord) => {
+  let toggle = args
+  if (toggle == "on") {
+    config.rotator = "yes"
+    offon = "on"
+  }
+  if (toggle == "off") {
+    config.rotator = "no"
+    offon = "off"
+  }
+  
+  let embed = new discord.RichEmbed()
+  .setTitle("Status Rotator")
+  .setDescription(`Turning the rotator ${offon}.`)
+  .setTimestamp()
+  .setColor("BLUE")
+  .setThumbnail(bot.user.avatarURL)
+  
+  message.channel.send({embed: embed})
+}
+
+module.exports.help = {
+  name: "toggle"
+}
