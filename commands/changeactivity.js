fdaa4bb1612df5a5833a52298076e0afdcf69989
FHGDev <gideon.lingle14@gmail.com>
2018-04-23 13:44:00 -0500

    Update changeactivity.js

--- commands/changeactivity.js
+++ commands/changeactivity.js
@@ -3,11 +3,22 @@ const config = require('../config.json')
 module.exports.run = (bot, message, args, discord) => {
 if (config.ownerids.includes(message.author.id)) {
   if (!args) {
-    let embed=new discord.RichEmbed()
+    let embed = new discord.RichEmbed()
     .setTitle("Activity Changer")
-    .setDescription("Okay! I changed my activity back to normal.")
-  
-      message.channel.send(
+    .setDescription(":thumbsup: Okay! I changed my activity back to normal.")
+    .setTimestamp()
+    .setThumbnail(bot.user.avatarURL)
+    .setColor("BLUE")
+      message.channel.send({embed: embed}))
+    } else {
+      let em = new discord.RichEmbed()
+      let [game, type] = args
+      bot.user.setActivity(game, {type: type})
+      message.channel.send({embed: em})
     }
   }
+}
+
+module.exports.help = {
+  name: "changeactivity"
 }
