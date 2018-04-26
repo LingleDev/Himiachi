027fdac086706a3156cebd86a5d14c7bf725ae6b
FHGDev <gideon.lingle14@gmail.com>
2018-04-26 12:41:12 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,6 +50,7 @@ bot.on('message', message => {
   let cmd = bot.commands.get(mArray[0].slice(prefix.length))
   if (message.author.bot) return;
   if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(prefix)) return;
   
   if (cmd) {
     cmd.run(bot, message, args, discord)
