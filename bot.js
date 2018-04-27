1a14fb016442e6e28f58685c80d08d32634d55ff
FHGDev <gideon.lingle14@gmail.com>
2018-04-27 16:00:33 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,6 +53,7 @@ bot.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   
   if (cmd) {
+    if (config.ubl.includes(message.author.id)) return;
     cmd.run(bot, message, args, discord)
     console.log(`${message.author.username} used the ${message.content.split(" ")[0]} command.`)
     baselogger(bot, `**Command Run**\n\n**Command:** ${message.content.split(" ")[0]}\n**User:** ${message.author.tag}\n**Message:** ${message.content}\n**Guild:** ${message.guild.name}\n**Channel:** ${message.channel.name}`);
