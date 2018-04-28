8afca46362e220109549112659a7dc62135ed83e
FHGDev <gideon.lingle14@gmail.com>
2018-04-27 20:34:55 -0500

    Update annoy.js

--- commands/annoy.js
+++ commands/annoy.js
@@ -1,16 +1,20 @@
 module.exports.run = (bot, message, args, discord) => {
  let member = message.mentions.members.first();
- let em = new discord.RichEmbed()
-  .setTitle("Himiachi Annoy")
-  .setDescription(`Now annoying <@${member.id}>...`)
-  .setColor("RED")
-  .setThumbnail(bot.user.avatarURL)
-  .setFooter(message.author.username)
-  .setTimestamp()
-   bot.fetchUser(member.id).then(user => {
+ if (!member) {
+  message.channel.send("You need to mention someone!")
+ } else {
+  let em = new discord.RichEmbed()
+   .setTitle("Himiachi Annoy")
+   .setDescription(`Now annoying <@${member.id}>...`)
+   .setColor("RED")
+   .setThumbnail(bot.user.avatarURL)
+   .setFooter(message.author.username)
+   .setTimestamp()
+    bot.fetchUser(member.id).then(user => {
     user.send(`<@${user.id}> LOL! ${message.author.username} sent me to annoy you! <@${user.id}> <@${user.id}>`)
   })
    message.channel.send({embed: em})
+ }
 }
 
 module.exports.help = {
