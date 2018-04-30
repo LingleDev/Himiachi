98d580a85e09cd7155fe215607673e2cad298b66
FHGDev <gideon.lingle14@gmail.com>
2018-04-30 09:02:45 -0500

    Update refresh.js

--- commands/refresh.js
+++ commands/refresh.js
@@ -1,10 +1,16 @@
+const ownerids = require('../config.json').ownerids
+
 module.exports.run = (bot, message, args, discord) => {
+    if (ownerids.includes(message.author.id)) {
     let cmd = args.join(" ")
     if (!args) {
     message.channel.send("You must provide a command name to reload.")
 } else {
-      delete require.cache[require.resolve(`./${cmd}.js`)]
-      message.channel.send(`Command ${cmd} has been reloaded.`)
+          delete require.cache[require.resolve(`./${cmd}.js`)]
+            message.channel.send(`Command ${cmd} has been reloaded.`)
+        }
+    } else {
+        message.channel.send("Owner only, you cannot use this command.")
     }
 }
 
