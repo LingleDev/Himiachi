3e762bb5948c166198dcb6718d97c962b33bbf1a
FHGDev <gideon.lingle14@gmail.com>
2018-04-30 09:00:50 -0500

    Update shutdown.js

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,14 +1,20 @@
+let config = require('../config.json')
+
 module.exports.run = (bot, message, args, discord) => {
-  let em = new discord.RichEmbed()
-  .setTitle("Himiachi Shutdown")
-  .setDescription(":wave: Shutting down :cry:")
-  .setTimestamp()
-  .setColor("BLACK")
-  message.channel.send({embed: em})
-  setTimeout(() => {
-    bot.destroy();
-    process.exit(143)
-  }, 5000)
+  if (config.ownerids.includes(message.author.id)) {
+    let em = new discord.RichEmbed()
+    .setTitle("Himiachi Shutdown")
+    .setDescription(":wave: Shutting down :cry:")
+    .setTimestamp()
+    .setColor("BLACK")
+    message.channel.send({embed: em})
+    setTimeout(() => {
+      bot.destroy();
+      process.exit(143)
+    }, 5000)
+  } else {
+    message.channel.send("Owner only, you cannot use this command.")
+  }
 }
 
 module.exports.help = {
