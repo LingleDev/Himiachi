6ee2a51fe4b78c7b60270febce57cb8671bb90d8
FHGDev <gideon.lingle14@gmail.com>
2018-05-09 22:01:19 -0500

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,20 @@
+module.exports.run = (bot, message, args, discord) => {
+  require('snekfetch').get('https://meme-api.explosivenight.us/v1/random/?type=json')
+  .then(response => {
+    let em = new discord.RichEmbed()
+    .setTitle("Himiachi Random Memes")
+    .setImage(response.body.url)
+    .setDescription("I got a meme for you!")
+    .setTimestamp()
+    .setFooter("Random Meme!")
+    if (response.body.status == "200 OK") {
+      message.channel.send({embed: em})
+    } else {
+      message.channel.send("I couldn't get a meme...")
+    }
+  })
+}
+
+module.exports.help = {
+  name: "meme"
+}
