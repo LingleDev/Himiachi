0ede7c361567bea043c40c1cd6bf5734d3a21dea
FHGDev <gideon.lingle14@gmail.com>
2018-05-22 18:28:27 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 module.exports.run = (bot, message, args, discord) => {
  let em = new discord.RichEmbed()
   .setTitle("Himiachi Invite")
-  .setDescription(`Invite me [here](https://discordapp.com/api/oauth2/authorize?client_id=445958003582173204&permissions=0&redirect_uri=https%3A%2F%2Fbot.himiachi.ml%2Fwebsite%2Fhome&response_type=code&scope=bot%20guilds "invite Himiachi").`)
+  .setDescription(`Invite me [here](https://discordapp.com/api/oauth2/authorize?client_id=445958003582173204&permissions=8&redirect_uri=https%3A%2F%2Fbot.himiachi.ml%2Fwebsite%2Fhome&response_type=code&scope=bot%20guilds "invite Himiachi").`)
   .setColor("GREEN")
   .setThumbnail(bot.user.avatarURL)
   .setFooter(message.author.username)
