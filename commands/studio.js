a9d6bfec0a1bec231de7ae870484f318ee656918
FHGDev <gideon.lingle14@gmail.com>
2018-05-24 19:35:06 -0500

    Update studio.js

--- commands/studio.js
+++ commands/studio.js
@@ -1,15 +1,16 @@
 module.exports.run = (bot, message, args, discord) => {
- let em = new discord.RichEmbed()
- .setTitle("Our Studio!")
- .setDescription("Welp this is our studio!)
- .addField("Here's an invite!", `https://discord.gg/Ch5HEjj`)
- .setFooter(message.author.username)
- .setTimestamp()
+  let em = new discord.RichEmbed()
+  .setTitle("Our Studio!")
+  .setDescription("Welp this is our studio!")
+  .addField("Here's an invite!", `Join our server [here](https://discord.gg/Ch5HEjj "join the studio")`)
+  .setFooter(`Requested by ${message.author.username}`)
+  .setTimestamp()
+  .setColor("BLUE")
  
-    message.channel.send({embed: em})
-    }
+  message.channel.send({embed: em})
+}
     
-    module.exports.help = {
- name: "studio",
- usage: ".studio"
- }
+module.exports.help = {
+  name: "studio",
+  usage: ".studio"
+}
